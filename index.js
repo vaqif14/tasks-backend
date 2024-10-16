cbb22b4c1f8c9b38d0e3ac8970a33d6a15ce7456
Vaqif <vaqif14@gmail.com>
2024-10-16 20:09:43 +0400

    upd

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ connectDB();
 
 app.use(express.json());
 app.use("/api", routes);
-
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
